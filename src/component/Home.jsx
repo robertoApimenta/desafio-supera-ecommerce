6210be16742d4bb3ce7ff20acf1296211e6571b2
robertoApimenta <roberto.uft@gmail.com>
2022-04-14 15:53:17 -0300

    Gif readme

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,10 +5,10 @@ const Home = () => {
     return (
         <div className="hero">
             <div className="card bg-dark text-white border-0">
-                <img src="/assets/bg.jpg" className="card-img" alt="Background" />
+                <img src="/assets/bg.jpg" height="300px" className="card-img" alt="Background" />
                 <div className="card-img-overlay d-flex flex-column justify-content-center">
                     <div className="container">
-                        <h5 className="card-title display-3 fw-bolder mb-0">NOVOS GAMES</h5>
+                        <h5 className="card-title display-3 fw-bolder mb-0">NEW GAMES</h5>
                         <p className="card-text lead fs-2">TODAS AS NOVIDADES</p>
                     </div>
                 </div>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -10,14 +10,14 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">SELECT</Link>
+                    <Link className="navbar-brand" to="/">E-COMMERCE</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">
+                                <Link className="nav-link" aria-current="page" to="/">
                                     Home
                                 </Link>
                             </li>
@@ -27,21 +27,21 @@ export default function Navbar() {
                                 </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/sobre">
+                                <Link className="nav-link" to="/">
                                     Sobre
                                 </Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/contato">
+                                <Link className="nav-link" to="/">
                                     Contato
                                 </Link>
                             </li>
                         </ul>
                         <div className="buttons">
-                            <Link to="/login" className="btn btn-outline-dark">
+                            <Link to="/" className="btn btn-outline-dark">
                                 <i className="fa fa-sign-in me-1"></i>Login
                             </Link>
-                            <Link to="/cadastrar" className="btn btn-outline-dark ms-2">
+                            <Link to="/" className="btn btn-outline-dark ms-2">
                                 <i className="fa fa-user-plus me-1"></i>Cadastrar
                             </Link>
                             <Link to="/carrinho" className="btn btn-outline-dark ms-2">
